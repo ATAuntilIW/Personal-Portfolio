53f13d7c985dd76a26f65c394d58a2eef6e46d12
berradamm <88903339+ATAuntilIW@users.noreply.github.com>
2022-11-17 09:31:12 +0100

    orders & search orders

--- orders.php
+++ orders.php
@@ -37,6 +37,7 @@ if(!isset($user_id)){
                 while($fetch_orders=mysqli_fetch_assoc($order_query)){
 
             ?>
+            <div class="box">
             <p> Placed on: <span><?php echo $fetch_orders['placed_on'];?></span></p>
             <p> Name: <span><?php echo $fetch_orders['name'];?></span></p>
             <p> Number: <span><?php echo $fetch_orders['number'];?></span></p>
@@ -46,7 +47,7 @@ if(!isset($user_id)){
             <p>Your orders: <span><?php echo $fetch_orders['total_products'];?></span></p>
             <p>total price: <span><?php echo $fetch_orders['total_price'];?>€</span></p>
             <p>Payment status: <span style="color: <?php if($fetch_orders['payment_status']=='pending'){echo 'red';}else {echo 'green';} ?>"><?php  echo $fetch_orders['payment_status'];?></span></p>
-
+            </div>
               <?php 
        
                     
